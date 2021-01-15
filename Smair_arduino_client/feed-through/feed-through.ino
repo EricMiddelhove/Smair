3169e06c84940f57217aeae0efcbc2299cb52434
Eric Middelhove <eric.middelhove@icloud.com>
2021-01-15 01:29:08 +0100

    added feed through sketch to repo

--- Smair_arduino_client/feed-through/feed-through.ino
+++ Smair_arduino_client/feed-through/feed-through.ino
@@ -32,7 +32,7 @@ SoftwareSerial mySerial(2, 3); // RX, TX
 void setup()
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
